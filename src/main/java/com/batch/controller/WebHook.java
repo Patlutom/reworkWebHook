682bcb6cc5eab85e3509c96d14fb3cb49321078c
ganesh <ganesh.m@proglint.com>
2023-05-22 17:32:08 +0530

    github hook

--- src/main/java/com/batch/controller/WebHook.java
+++ src/main/java/com/batch/controller/WebHook.java
@@ -16,6 +16,11 @@ public class WebHook {
 	public ResponseEntity<String> callWebHook(@RequestBody String requestBody) {
 		
 		System.out.println("Print the webheook" +requestBody + " Result*************");
+		System.out.println("Print the webheook" +requestBody + " Result*************");
+		System.out.println("Print the webheook" +requestBody + " Result*************");
+		System.out.println("Print the webheook" +requestBody + " Result*************");
+		System.out.println("Print the webheook" +requestBody + " Result*************");
+		System.out.println("Print the webheook" +requestBody + " Result*************");
 		return new ResponseEntity<String>(requestBody, HttpStatus.OK);
 	}
 
