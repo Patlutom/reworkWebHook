9e22bf8723047fde33f2a8a84c854f6457820de3
ganesh <ganesh.m@proglint.com>
2023-05-22 17:36:51 +0530

    remove code

--- src/main/java/com/batch/controller/WebHook.java
+++ src/main/java/com/batch/controller/WebHook.java
@@ -17,10 +17,6 @@ public class WebHook {
 		
 		System.out.println("Print the webheook" +requestBody + " Result*************");
 		System.out.println("Print the webheook" +requestBody + " Result*************");
-		System.out.println("Print the webheook" +requestBody + " Result*************");
-		System.out.println("Print the webheook" +requestBody + " Result*************");
-		System.out.println("Print the webheook" +requestBody + " Result*************");
-		System.out.println("Print the webheook" +requestBody + " Result*************");
 		return new ResponseEntity<String>(requestBody, HttpStatus.OK);
 	}
 
